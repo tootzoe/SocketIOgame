09a86e260d0601c17eecd5564e3e2abe5d398c6e
TOOTzoe <toot@tootzoe.com>
2024-12-17 01:48:37 +0700

    Add message send and display functions....

--- Source/SocketIOgame/Private/SocketIOManager.cpp
+++ Source/SocketIOgame/Private/SocketIOManager.cpp
@@ -3,22 +3,26 @@
 
 #include "SocketIOgame/Public/SocketIOManager.h"
 
-
+#include <string>
+#include <vector>
 
 #include "WebSocketsModule.h"
 #include "IWebSocket.h"
 
 
 
+ FWssStatusSignature USocketIOManager::OnWssStatusChged  ;
+
 USocketIOManager::USocketIOManager() :
     _mainSocket(nullptr)
 {
+    OnWssStatusChged.Clear();
+    OnWssStatusChged.AddUniqueDynamic(this, &USocketIOManager::WssStatusEvent);
 
 }
 
 USocketIOManager::~USocketIOManager()
 {
-
     //UE_LOG(LogTemp, Warning, TEXT("USocketIOManager deconstructed............"));
    // DisconnectedFromServer();
 
@@ -28,19 +32,22 @@ USocketIOManager::~USocketIOManager()
 void USocketIOManager::InitializeSocketIO()
 {
     if(!   FModuleManager::Get().IsModuleLoaded("WebSockets") ){
-        GEngine->AddOnScreenDebugMessage(-1, 16.f, FColor::Green, TEXT("Load WebSockets module now...."));
         FModuleManager::Get().LoadModule("WebSockets");
     }
 
+}
 
-  // ensureAlwaysMsgf()
+
+static void FStringToUin8Arr(const FString& Message, TArray<uint8>& binDataArr )
+{
+    int32 Utf8Length = FTCHARToUTF8_Convert::ConvertedLength(*Message, Message.Len());
+    binDataArr.SetNumUninitialized(Utf8Length);
+    FTCHARToUTF8_Convert::Convert((UTF8CHAR*)binDataArr.GetData(), binDataArr.Num(), *Message, Message.Len());
 }
 
 int32 USocketIOManager::ConnectToUrl(const FString &Url)
 {
-    GEngine->AddOnScreenDebugMessage(-1, 16.f, FColor::Red, TEXT("Try connect to : %s " )   );
-
-    _mainSocket =  FWebSocketsModule::Get().CreateWebSocket(*Url); // "ws://127.0.0.1:8080"
+    _mainSocket =  FWebSocketsModule::Get().CreateWebSocket(*Url, TEXT("wss") ); // "ws://127.0.0.1:8080"
 
     ensureAlwaysMsgf(_mainSocket.IsValid() , TEXT("Create WebSocket Failed....")  );
 
@@ -48,41 +55,65 @@ int32 USocketIOManager::ConnectToUrl(const FString &Url)
     _mainSocket->OnConnected().AddLambda([]() -> void {
         // This code will run once connected.
          GEngine->AddOnScreenDebugMessage(-1, 16.f, FColor::Green, TEXT("OnConnected ok.... " )   );
+          UE_LOG(LogTemp, Warning, TEXT("OnConnected() called...."));
+           TArray<uint8> binDataArr;
+          OnWssStatusChged.Broadcast( E_Connected , binDataArr);
     });
 
     _mainSocket->OnConnectionError().AddLambda([](const FString & Error) -> void {
         // This code will run if the connection failed. Check Error to see what happened.
          GEngine->AddOnScreenDebugMessage(-1, 16.f, FColor::Red, Error  );
+         UE_LOG(LogTemp, Warning, TEXT("OnConnectionError().... Err: %s"), *Error);
+
+          TArray<uint8> binDataArr;
+          FStringToUin8Arr(Error , binDataArr);
+          OnWssStatusChged.Broadcast( E_ConnectionError ,  binDataArr );
     });
 
     _mainSocket->OnClosed().AddLambda([](int32 StatusCode, const FString& Reason, bool bWasClean) -> void {
         // This code will run when the connection to the server has been terminated.
         // Because of an error or a call to Socket->Close().
-         GEngine->AddOnScreenDebugMessage(-1, 16.f, FColor::Red, TEXT("OnClosed called...... " )   );
+         UE_LOG(LogTemp, Warning, TEXT("OnClosed() called....") );
+         TArray<uint8> binDataArr;
+        OnWssStatusChged.Broadcast( E_ServerClosed, binDataArr);
     });
 
     _mainSocket->OnMessage().AddLambda([](const FString & Message) -> void {
         // This code will run when we receive a string message from the server.
-         GEngine->AddOnScreenDebugMessage(-1, 16.f, FColor::Green, Message   );
+        // UE_LOG(LogTemp, Warning, TEXT("TxtData: len= %ld , %s"), Message.Len()  , *Message);
+
+         TArray<uint8> binDataArr;
+         FStringToUin8Arr(Message , binDataArr);
+         OnWssStatusChged.Broadcast( E_ConnectionError ,  binDataArr );
     });
 
     _mainSocket->OnRawMessage().AddLambda([](const void* Data, SIZE_T Size, SIZE_T BytesRemaining) -> void {
         // This code will run when we receive a raw (binary) message from the server.
-         GEngine->AddOnScreenDebugMessage(-1, 16.f, FColor::Blue, TEXT("Binary data Arrived.... " )   );
+        UE_LOG(LogTemp, Warning, TEXT("--BinData: len= %ld , BytesRemaining = %ld"), Size , BytesRemaining);
+       // UE_LOG(LogTemp, Warning, TEXT("BinData: %hs "), (char*)Data);
+
+        TArray<uint8> binDataArr;
+        binDataArr.Append((const unsigned char*)Data, Size);
+
+         // const FString str =  UTF8_TO_TCHAR(binDataArr.GetData());
+       // UE_LOG(LogTemp, Warning, TEXT("BinData: %s "), *str);
+
+         OnWssStatusChged.Broadcast( E_BinaryData ,  binDataArr );
+
     });
 
-    _mainSocket->OnMessageSent().AddLambda([](const FString& MessageString) -> void {
-        // This code is called after we sent a message to the server.
-         GEngine->AddOnScreenDebugMessage(-1, 16.f, FColor::Yellow, TEXT("OnMessageSent...... " )   );
+    _mainSocket->OnMessageSent().AddLambda([](const FString& Message) -> void {
+        // This code is called after we sent a message(*ONLY Text* msg) to the server.
+        // UE_LOG(LogTemp, Warning, TEXT("OnMessageSent() called.... msg: %s"), *Message);
+
+         TArray<uint8> binDataArr;
+           FStringToUin8Arr(Message , binDataArr);
+         OnWssStatusChged.Broadcast( E_TextMsgSent ,  binDataArr );
+
     });
-
-
 
 
     _mainSocket.Get()->Connect();
-
-     GEngine->AddOnScreenDebugMessage(-1, 16.f, FColor::Red, TEXT("WebSocket Connecting....")  );
-
 
     return 0;
 }
@@ -100,17 +131,19 @@ void USocketIOManager::DisconnectedFromServer()
 void USocketIOManager::SendTxtMsg(const FString &Msg)
 {
     ensureAlwaysMsgf(_mainSocket.IsValid(),TEXT("WebSocket invalid...."));
-
     _mainSocket.Get()->Send(*Msg);
-
-    if( GEngine ){
-        GEngine->AddOnScreenDebugMessage(-1, 16.f, FColor::Cyan, TEXT("Msg sent...."));
-    }
 }
 
 void USocketIOManager::SendBinData(const TArray<uint8> &BinData)
 {
+    ensureAlwaysMsgf(_mainSocket.IsValid(),TEXT("WebSocket invalid...."));
 
+    _mainSocket.Get()->Send(BinData.GetData(), sizeof(uint8) * BinData.Num(), true);
+}
+
+void USocketIOManager::Uint8ArrToString(const TArray<uint8> &BinData , FString & RslStr)
+{
+     RslStr = FString( BinData.Num(), UTF8_TO_TCHAR(BinData.GetData()));
 }
 
 
--- Source/SocketIOgame/Public/SocketIOManager.h
+++ Source/SocketIOgame/Public/SocketIOManager.h
@@ -13,6 +13,29 @@
 class IWebSocket;
 class IWebSocketsManager;
 
+
+
+
+#if 1
+UENUM(BlueprintType)
+enum  EWebSocketStatus : uint8
+{
+    E_Disconnected UMETA(DisplayName="Disconnected"),
+    E_Connecting UMETA(DisplayName="Connecting"),
+    E_Connected UMETA(DisplayName="Connected"),
+    E_ConnectionError UMETA(DisplayName="Connection Error"),
+    E_ServerClosed UMETA(DisplayName="Server Closed"),
+    E_TextMsg UMETA(DisplayName="Text Msg"),
+    E_TextMsgSent UMETA(DisplayName="Text Msg Sent"),
+    E_BinaryData UMETA(DisplayName="Binary Data"),
+    //
+    E_Max UMETA(Hidden)
+};
+#endif
+
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FWssStatusSignature,  EWebSocketStatus  , WssStatus, const TArray<uint8>& ,  BinDataA);
+
 /**
  * 
  */
@@ -38,11 +61,21 @@ class SOCKETIOGAME_API USocketIOManager : public UObject
     void SendTxtMsg( const FString& Msg );
     UFUNCTION(BlueprintCallable, Category = "TOOT")
     void SendBinData( const TArray<uint8>& BinData );
+    //
+    UFUNCTION(BlueprintImplementableEvent, Category = "TOOT")
+    void WssStatusEvent( EWebSocketStatus EStatus,  const TArray<uint8>& BinData );
+    //
+    UFUNCTION(BlueprintCallable, Category = "TOOT")
+    void Uint8ArrToString( const TArray<uint8>& BinData , FString &RslStr );
 
+    // UPROPERTY(BlueprintAssignable, Category="TOOT")
+    // FWssStatusSignature OnWssStatusChged;
+
+   // UFUNCTION()
 
  private:
 
-
+    static FWssStatusSignature OnWssStatusChged;
     TSharedPtr <IWebSocket> _mainSocket;
 
 };
